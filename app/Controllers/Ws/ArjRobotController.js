5de291922ab6db44173a746d1321f27c008beefb
root <root@robot.arjrobot.com>
2020-04-23 04:48:33 +0000

    init

---
+++ app/Controllers/Ws/ArjRobotController.js
@@ -0,0 +1,87 @@
+  'use strict'
+
+class ArjRobotController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+ }
+
+ onHoy(data)
+{
+ this.socket.broadcastToAll('hoy',data);
+}
+ onTest(data)
+ {
+   console.log('Subscripcion ' + data);
+   this.socket.broadcastToAll('subscripcion', 'Hola desde el server');
+}
+  // Carro
+  onAtras(data)
+  {
+    console.log('Atras ' + data);
+    this.socket.broadcastToAll('changeAtras',data);
+
+  }
+  onAdelante(data)
+  {
+    console.log('Adelante ' + data);
+    this.socket.broadcastToAll('changeAdelante',data);
+  }
+  onIzquierda(data)
+  {
+    console.log('Izquierda ' + data);
+    this.socket.broadcastToAll('changeIzquierda',data);
+
+
+  }
+  onDerecha(data)
+  {
+    console.log('Derecha ' + data);
+    this.socket.broadcastToAll('changeDerecha',data);
+
+  }
+
+  //Pinza
+  onAgarrar(data)
+  {
+    console.log('Agarre ' + data);
+    this.socket.broadcastToAll('changeAgarre',data);
+
+  }
+  onSoltar(data)
+  {
+    console.log('Soltar ' + data);
+    this.socket.broadcastToAll('changeSoltar',data);
+
+  }
+
+  // Movimiento Brazo
+  onBAbajo(data)
+  {
+    console.log('Abajo Brazo ' + data);
+    this.socket.broadcastToAll('changeBAbajo',data);
+
+  }
+  onBArriba(data)
+  {
+    console.log('Arriba Brazo ' + data);
+    this.socket.broadcastToAll('changeBArriba',data);
+
+
+  }
+  onBIzquierda(data)
+  {
+    console.log('Izquierda Brazo ' + data);
+    this.socket.broadcastToAll('changeBIzquierda',data);
+
+  }
+  onBDerecha(data)
+  {
+    console.log('Derecha Brazo ' + data);
+    this.socket.broadcastToAll('changeBDerecha',data);
+
+  }
+}
+
+module.exports = ArjRobotController
+
---
+++ start/routes.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+Route.on('').render('welcome');
+Route.on('/').render('welcome');
+Route.group(() => {
+  Route.get('location','cloud/MongoController.location');
+  Route.post('location','cloud/MongoController.savelocation');
+  Route.get('fecha','cloud/MongoController.fecha');
+  Route.post('fecha','cloud/MongoController.savefecha');
+  Route.get('distancia','cloud/MongoController.distancia');
+  Route.post('distancia','cloud/MongoController.savedistancia');
+}).prefix('api/mongo')
+
+Route.group(() => {
+  Route.get('location','cloud/FirebaseController.location');
+  Route.post('location','cloud/FirebaseController.savelocation');
+  Route.get('fecha','cloud/FirebaseController.fecha');
+  Route.post('fecha','cloud/FirebaseController.savefecha');
+  Route.get('distancia','cloud/FirebaseController.distancia');
+  Route.post('distancia','cloud/FirebaseController.savedistancia');
+}).prefix('api/firebase')
+
+// Route.get('index', 'Firebase/SavedatumController.index');
+// Route.get('location', 'Firebase/SavedatumController.location');
+// Route.post('location', 'Firebase/SavedatumController.savelocation');
+Route.group(() =>{
+  Route.post('login', 'UserController.login')
+  Route.post('register', 'UserController.register')
+  Route.get('mostrar/:id', 'UserController.show')
+}).prefix('/users')
